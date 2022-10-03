173e01a02c388fcdff33267ae332968fd3e13e91
Constantine <cfragkos@healthier-app.com>
2022-10-03 03:17:22 +0300

    build fixes

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "Building Windows executable..."
-deno compile --allow-read --allow-write --output ./build/TripAdvisorCleaner --target x86_64-pc-windows-msvc index.ts 
+deno compile --allow-read --allow-write --output ./build/windows/TripAdvisorCleaner --target x86_64-pc-windows-msvc index.ts 
+cd ./build/windows
+zip -r windows.zip .
+cd ../../
 echo "Building Linux executable..."
-deno compile --allow-read --allow-write --output ./build/TripAdvisorCleaner --target x86_64-unknown-linux-gnu index.ts
+deno compile --allow-read --allow-write --output ./build/linux/TripAdvisorCleaner --target x86_64-unknown-linux-gnu index.ts
+cd ./build/linux
+zip -r linux.zip .
